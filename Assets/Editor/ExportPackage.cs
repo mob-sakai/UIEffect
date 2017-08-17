ac4f4d6a0d7eca29426ccf418576d46cac271338
Takashi Sakai <sakai@mobcast.jp>
2017-08-17 12:30:22 +0900

    Automatically export package

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace UnityEditor.UI
+{
+	public static class ExportPackage
+	{
+		const string kPackageName = "UIEffect.unitypackage";
+		static readonly string[] kAssetPathes = {
+			"Assets/UIEffect",
+		};
+
+		[MenuItem ("Export Package/" + kPackageName)]
+		[InitializeOnLoadMethod]
+		static void Export ()
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
+			
+			AssetDatabase.ExportPackage (kAssetPathes, kPackageName, ExportPackageOptions.Recurse | ExportPackageOptions.Default);
+			UnityEngine.Debug.Log ("Export successfully : " + kPackageName);
+		}
+	}
+}
