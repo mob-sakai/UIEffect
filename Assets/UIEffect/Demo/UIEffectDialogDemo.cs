9ec1982a7188da2354f6cf5ae4ced944462c85ba
Takashi Sakai <sakai861104@gmail.com>
2018-01-05 01:53:01 +0900

    Demo: Add transition blur button & dialog with captured image.

---
+++ Assets/UIEffect/Demo/UIEffectDialogDemo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIEffectDialogDemo : MonoBehaviour {
+
+	public void Open()
+	{
+		gameObject.SetActive(true);
+		GetComponent<Animator>().SetTrigger("Open");
+	}
+
+	public void Close()
+	{
+		GetComponent<Animator>().SetTrigger("Close");
+	}
+
+	public void Closed()
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void CaptureBackground()
+	{
+		GetComponentInChildren<UIEffectCapturedImage>().Capture();
+	}
+}
