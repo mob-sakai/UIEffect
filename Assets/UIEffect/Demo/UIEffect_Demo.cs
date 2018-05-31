81d64e414de8211268803145f36c2c89103418db
mob-sakai <sakai@mobcast.jp>
2018-05-31 14:14:21 +0900

    Clean up demo

---
+++ Assets/UIEffect/Demo/UIEffect_Demo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Coffee.UIExtensions
+{
+	public class UIEffect_Demo : MonoBehaviour
+	{
+		[SerializeField] RectMask2D mask;
+
+		// Use this for initialization
+		void Start()
+		{
+			if (mask)
+			{
+				mask.enabled = true;
+			}
+		}
+		
+		public void SetTimeScale(float scale)
+		{
+			Time.timeScale = scale;
+		}
+
+		public void Open(Animator anim)
+		{
+			anim.GetComponentInChildren<UIEffectCapturedImage>().Capture();
+			anim.gameObject.SetActive(true);
+			anim.SetTrigger("Open");
+		}
+
+		public void Close(Animator anim)
+		{
+			anim.SetTrigger("Close");
+		}
+
+		public void Capture(Animator anim)
+		{
+			anim.GetComponentInChildren<UIEffectCapturedImage>().Capture();
+			anim.SetTrigger("Capture");
+		}
+	}
+}
