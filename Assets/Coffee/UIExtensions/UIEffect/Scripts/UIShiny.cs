b1588400d589cf0ec7144983051186d0b965c738
mob-sakai <sakai@mobcast.jp>
2018-05-31 17:31:21 +0900

    close #80; UIShiny: Play effect from script/inspector

--- Assets/Coffee/UIExtensions/UIEffect/Scripts/UIShiny.cs
+++ Assets/Coffee/UIExtensions/UIEffect/Scripts/UIShiny.cs
@@ -40,6 +40,12 @@ namespace Coffee.UIExtensions
 		[SerializeField][Range(0, 1)] float m_Brightness = 1f;
 		[SerializeField][Range(0, 1)] float m_Highlight = 1;
 		[SerializeField] Material m_EffectMaterial;
+		[Space]
+		[SerializeField] bool m_Play = false;
+		[SerializeField] bool m_Loop = false;
+		[SerializeField][Range(0.1f, 10)] float m_Duration = 1;
+		[SerializeField][Range(0, 10)] float m_LoopDelay = 1;
+		[SerializeField] AnimatorUpdateMode m_UpdateMode = AnimatorUpdateMode.Normal;
 
 
 		//################################
@@ -53,44 +59,120 @@ namespace Coffee.UIExtensions
 		/// <summary>
 		/// Location for shiny effect.
 		/// </summary>
-		public float location { get { return m_Location; } set { m_Location = Mathf.Clamp(value, 0, 1); _SetDirty(); } }
+		public float location
+		{
+			get { return m_Location; }
+			set
+			{ 
+				value = Mathf.Clamp(value, 0, 1);
+				if (!Mathf.Approximately(m_Location, value))
+				{
+					m_Location = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Width for shiny effect.
 		/// </summary>
-		public float width { get { return m_Width; } set { m_Width = Mathf.Clamp(value, 0, 1); _SetDirty(); } }
+		public float width
+		{
+			get { return m_Width; }
+			set
+			{
+				value = Mathf.Clamp(value, 0, 1);
+				if (!Mathf.Approximately(m_Width, value))
+				{
+					m_Width = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Softness for shiny effect.
 		/// </summary>
-		public float softness { get { return m_Softness; } set { m_Softness = Mathf.Clamp(value, 0.01f, 1); _SetDirty(); } }
+		public float softness
+		{
+			get { return m_Softness; }
+			set
+			{
+				value = Mathf.Clamp(value, 0.01f, 1);
+				if (!Mathf.Approximately(m_Softness, value))
+				{
+					m_Softness = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Alpha for shiny effect.
 		/// </summary>
-		[System.Obsolete ("Use brightness instead (UnityUpgradable) -> brightness")]
-		public float alpha { get { return m_Brightness; } set { m_Brightness = Mathf.Clamp(value, 0, 1); _SetDirty(); } }
+		[System.Obsolete("Use brightness instead (UnityUpgradable) -> brightness")]
+		public float alpha
+		{
+			get { return m_Brightness; }
+			set
+			{
+				value = Mathf.Clamp(value, 0, 1);
+				if (!Mathf.Approximately(m_Brightness, value))
+				{
+					m_Brightness = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Brightness for shiny effect.
 		/// </summary>
-		public float brightness { get { return m_Brightness; } set { m_Brightness = Mathf.Clamp(value, 0, 1); _SetDirty(); } }
+		public float brightness
+		{
+			get { return m_Brightness; }
+			set
+			{
+				value = Mathf.Clamp(value, 0, 1);
+				if (!Mathf.Approximately(m_Brightness, value))
+				{
+					m_Brightness = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Highlight factor for shiny effect.
 		/// </summary>
-		public float highlight { get { return m_Highlight; } set { m_Highlight = Mathf.Clamp(value, 0, 1); _SetDirty(); } }
+		public float highlight
+		{
+			get { return m_Highlight; }
+			set
+			{
+				value = Mathf.Clamp(value, 0, 1);
+				if (!Mathf.Approximately(m_Highlight, value))
+				{
+					m_Highlight = value;
+					_SetDirty();
+				}
+			}
+		}
 
 		/// <summary>
 		/// Rotation for shiny effect.
 		/// </summary>
 		public float rotation
 		{
-			get
+			get { return m_Rotation; }
+			set
 			{
-				return m_Rotation;
+				if (!Mathf.Approximately(m_Rotation, value))
+				{
+					m_Rotation = value;
+					_SetDirty();
+				}
 			}
-			set { if (!Mathf.Approximately(m_Rotation, value)) { m_Rotation = value; _SetDirty(); } }
 		}
 
 		/// <summary>
@@ -99,10 +181,36 @@ namespace Coffee.UIExtensions
 		public virtual Material effectMaterial { get { return m_EffectMaterial; } }
 
 		/// <summary>
+		/// Play shinning on enable.
+		/// </summary>
+		public bool play { get { return m_Play; } set { m_Play = value; } }
+
+		/// <summary>
+		/// Play shinning loop.
+		/// </summary>
+		public bool loop { get { return m_Loop; } set { m_Loop = value; } }
+
+		/// <summary>
+		/// Shinning duration.
+		/// </summary>
+		public float duration { get { return m_Duration; } set { m_Duration = Mathf.Max(value, 0.1f); } }
+
+		/// <summary>
+		/// Delay on loop.
+		/// </summary>
+		public float loopDelay { get { return m_LoopDelay; } set { m_LoopDelay = Mathf.Max(value, 0); } }
+
+		/// <summary>
+		/// Shinning update mode.
+		/// </summary>
+		public AnimatorUpdateMode updateMode { get { return m_UpdateMode; } set { m_UpdateMode = value; } }
+
+		/// <summary>
 		/// This function is called when the object becomes enabled and active.
 		/// </summary>
 		protected override void OnEnable()
 		{
+			_time = 0;
 			graphic.material = effectMaterial;
 			base.OnEnable();
 		}
@@ -130,19 +238,19 @@ namespace Coffee.UIExtensions
 					return;
 
 				var mat = GetMaterial(shaderName);
-				if(m_EffectMaterial == mat && graphic.material == mat)
+				if (m_EffectMaterial == mat && graphic.material == mat)
 					return;
 
 				graphic.material = m_EffectMaterial = mat;
 				EditorUtility.SetDirty(this);
 				EditorUtility.SetDirty(graphic);
-				EditorApplication.delayCall +=AssetDatabase.SaveAssets;
+				EditorApplication.delayCall += AssetDatabase.SaveAssets;
 			};
 		}
 
 		public static Material GetMaterial(string shaderName)
 		{
-			string name = Path.GetFileName (shaderName);
+			string name = Path.GetFileName(shaderName);
 			return AssetDatabase.FindAssets("t:Material " + name)
 				.Select(x => AssetDatabase.GUIDToAssetPath(x))
 				.SelectMany(x => AssetDatabase.LoadAllAssetsAtPath(x))
@@ -156,8 +264,7 @@ namespace Coffee.UIExtensions
 		/// </summary>
 		public override void ModifyMesh(VertexHelper vh)
 		{
-
-			if (!IsActive())
+			if (!isActiveAndEnabled)
 				return;
 
 			// rect.
@@ -181,8 +288,8 @@ namespace Coffee.UIExtensions
 				nomalizedPos = localMatrix * vertex.position;
 
 				vertex.uv1 = new Vector2(
-					_PackToFloat(Mathf.Clamp01(nomalizedPos.y), softness, width, brightness),
-					_PackToFloat(location, highlight)
+					Packer.ToFloat(Mathf.Clamp01(nomalizedPos.y), softness, width, brightness),
+					Packer.ToFloat(location, highlight)
 				);
 
 				vh.SetUIVertex(vertex, i);
@@ -194,104 +301,42 @@ namespace Coffee.UIExtensions
 		/// </summary>
 		public void Play()
 		{
-			Play(1);
+			_time = 0;
+			m_Play = true;
 		}
 
-		/// <summary>
-		/// Play effect.
-		/// </summary>
-		public void Play(float duration)
-		{
-			StopAllCoroutines();
-			StartCoroutine(CoPlay(duration, AnimatorUpdateMode.Normal));
-		}
-
-		/// <summary>
-		/// Play effect.
-		/// </summary>
-		public void Play(float duration, AnimatorUpdateMode updateMode)
-		{
-			StopAllCoroutines();
-			StartCoroutine(CoPlay(duration, updateMode));
-		}
 
 		//################################
 		// Private Members.
 		//################################
+		float _time = 0;
+
+		void Update()
+		{
+			if (!m_Play || !Application.isPlaying)
+			{
+				return;
+			}
+
+			_time += m_UpdateMode == AnimatorUpdateMode.UnscaledTime
+					? Time.unscaledDeltaTime
+					: Time.deltaTime;
+			location = _time / m_Duration;
+
+			if (m_Duration <= _time)
+			{
+				m_Play = m_Loop;
+				_time = m_Loop ? -m_LoopDelay : 0;
+			}
+		}
+
 		/// <summary>
 		/// Mark the UIEffect as dirty.
 		/// </summary>
 		void _SetDirty()
 		{
-			if(graphic)
+			if (graphic)
 				graphic.SetVerticesDirty();
-		}
-
-		IEnumerator CoPlay(float duration, AnimatorUpdateMode updateMode = AnimatorUpdateMode.Normal)
-		{
-			float time = 0;
-			while (time < duration)
-			{
-				location = time / duration;
-				time += updateMode == AnimatorUpdateMode.UnscaledTime
-					? Time.unscaledDeltaTime
-					: Time.deltaTime;
-				yield return null;
-			}
-		}
-
-		/// <summary>
-		/// Pack 4 low-precision [0-1] floats values to a float.
-		/// Each value [0-1] has 64 steps(6 bits).
-		/// </summary>
-		static float _PackToFloat(float x, float y, float z, float w)
-		{
-			const int PRECISION = (1 << 6) - 1;
-			return (Mathf.FloorToInt(w * PRECISION) << 18)
-			+ (Mathf.FloorToInt(z * PRECISION) << 12)
-			+ (Mathf.FloorToInt(y * PRECISION) << 6)
-			+ Mathf.FloorToInt(x * PRECISION);
-		}
-
-		/// <summary>
-		/// Pack 2 low-precision [0-1] floats values to a float.
-		/// Each value [0-1] has 4096 steps(12 bits).
-		/// </summary>
-		static float _PackToFloat(float x, float y)
-		{
-			const int PRECISION = (1 << 12) - 1;
-			return (Mathf.FloorToInt(y * PRECISION) << 12)
-				+ Mathf.FloorToInt(x * PRECISION);
-		}
-
-
-		/// <summary>
-		/// Matrix2x3.
-		/// </summary>
-		struct Matrix2x3
-		{
-			public float m00, m01, m02, m10, m11, m12;
-
-			public Matrix2x3(Rect rect, float cos, float sin)
-			{
-				const float center = 0.5f;
-				float dx = -rect.xMin / rect.width - center;
-				float dy = -rect.yMin / rect.height - center;
-				m00 = cos / rect.width;
-				m01 = -sin / rect.height;
-				m02 = dx * cos - dy * sin + center;
-				m10 = sin / rect.width;
-				m11 = cos / rect.height;		
-				m12 = dx * sin + dy * cos + center;
-			}
-
-			public static Vector2 operator*(Matrix2x3 m, Vector2 v)
-			{
-				return new Vector2(
-					(m.m00 * v.x) + (m.m01 * v.y) + m.m02,
-					(m.m10 * v.x) + (m.m11 * v.y) + m.m12
-				);
-			}
 		}
 	}
 }
