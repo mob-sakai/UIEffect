d840a76759dc3ecb9171ad4bdab68828d696a412
mob-sakai <sakai@mobcast.jp>
2018-08-14 16:12:12 +0900

    Update demo

---
+++ Assets/Coffee/UIExtensions/UIEffect/Demo/UIEffect_Demo_Dialog.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Coffee.UIExtensions
+{
+	public class UIEffect_Demo_Dialog : MonoBehaviour
+	{
+		[SerializeField] Animator m_Animator;
+
+		public void Open()
+		{
+			gameObject.SetActive(true);
+		}
+
+		public void Close()
+		{
+			m_Animator.SetTrigger("Close");
+		}
+
+		public void Closed()
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
